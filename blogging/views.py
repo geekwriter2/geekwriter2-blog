8160b85b41035cabcb2a195b319e41f749121c9a
geekwriter2 <geekwriter@hotmail.com>
2021-02-06 10:07:44 -0800

    michael_mcdonald interim comitt l7

--- blogging/views.py
+++ blogging/views.py
@@ -2,30 +2,35 @@
 
 from django.shortcuts import render
 from django.http import Http404
+from django.template import loader
 from blogging.models import Category, Post
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
-def list_view(request):
-    context = {'posts': Post.objects.all()}
-    return render(request, 'blogging/list.html', context)
 
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    body = template.render(context)
+    return HttpResponse(body, content_type="text/html")
+
+"""
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'list.html', context)
+"""
 
 def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
     try:
-        post = Post.objects.get(pk=post_id)
+        post = published.get(pk=post_id)
     except Post.DoesNotExist:
         raise Http404
-
-    if request.method == "POST":
-        if request.POST.get("vote") == "Yes":
-            post.score += 1
-        else:
-            post.score -= 1
-        post.save()
-
-    context = {'poll': post}
-    return render(request, 'polling/detail.html', context)
-
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
